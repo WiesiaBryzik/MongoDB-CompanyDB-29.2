dfec5ef16751486d21a63703a792c9a6c5e43c73
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-03-07 16:56:49 +0100

    Add get, post, put & delete test

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
+// app.listen('8000', () => {
+//   console.log('Server is running on port: 8000');
+// });
+
+const server = app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
+
+module.exports = server;
